1539f9db51f8c1f137f8904e6d2488e00c77af7c
turagsarkar <turagsarkar@gmail.com>
2024-03-19 09:31:50 +0600

    Init Calculator

---
+++ Fundamental/Calculator.py
@@ -0,0 +1,10 @@
+#basic init calculator 
+
+a= 5 
+b= 3 
+
+print("The add of", a ,'+', b , 'is equal =',a+b)
+print("The sub of", a ,'-', b , 'is equal =',a-b)
+print("The div of", a ,'/', b , 'is equal =',a/b)
+print("The multi of", a ,'*', b , 'is equal =',a*b)
+print("The reminder of", a ,'/', b , 'is equal =',a%b)
