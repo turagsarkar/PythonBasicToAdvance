38f0096699731026e2281dcb9ef3540643f7163c
turagsarkar <turagsarkar@gmail.com>
2024-03-19 06:56:14 +0600

    print method and escape

--- Fundamental/print.py
+++ Fundamental/print.py
@@ -1,1 +1,14 @@
 print("hello world")
+# for comments you can use (# character)
+# like it's a fundamental course
+
+# using escape sequnce 
+
+print('It is a basic and \nadvance course') # \n for  new line
+
+# print(4+45)
+# print(45*80)
+# print()
+'''
+its also comments for multiple line 
+'''
