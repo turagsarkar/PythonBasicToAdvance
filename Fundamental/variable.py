a387cdd3e82b6fdddd2b51e6b210231b92d2a5b8
turagsarkar <turagsarkar@gmail.com>
2024-03-19 18:05:11 +0600

    TypeCasting

--- Fundamental/variable.py
+++ Fundamental/variable.py
@@ -5,4 +5,23 @@ print(type(a*b))
 
 name = 'Jayen'
 print(name)
-print(type(name))
+print(type(name)) # variable type 
+
+#typecasting method
+# Explicit typecasting 
+
+a= '2'
+b= '5'
+
+print(int(a)+int(b)) # Explicit typecasting
+
+#implicit typecasting 
+
+a = 10.45
+b= 3
+
+print(a*b)
+
+print(type(a*b))
+
+
